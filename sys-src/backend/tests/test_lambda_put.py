811acc7ebbee81e85bcf591fc9493d0b278d21ad
Darren Fürst <d.fuerst@oth-aw.de>
2022-06-01 10:55:46 +0200

    Fixed Lambda, Test is green, added make command for backend

--- sys-src/backend/tests/test_lambda_put.py
+++ sys-src/backend/tests/test_lambda_put.py
@@ -1,40 +1,58 @@
 import pytest
+import os
 from lambda_put import put
-from moto import mock_dynamodb2
+from moto import mock_dynamodb
 import boto3
 
 
 TableName = "testTable"
 
 
-@pytest.fixture
-def dynamodb_table():
-    with mock_dynamodb2():
-        dynamodb = boto3.client('dynamodb')
-        table = dynamodb.create_table(
-                TableName=TableName,
-                KeySchema=[
-                    {'KeyType': 'S', 'AttributeName': 'GameId'}
-                ],
-                AttributeDefinitions=[
-                    {'AttributeName': 'State', 'AttributeType': 'S'}
-                ]
-            )
-        
-        table.put_item(Item={
+@mock_dynamodb
+def test_put_lambda():
+    dynamodb = boto3.client('dynamodb')
+    dynamodb.create_table(
+            TableName=TableName,
+            KeySchema=[
+                {'KeyType': 'HASH', 'AttributeName': 'GameId'},
+            ],
+            AttributeDefinitions=[
+                {'AttributeName': 'GameId', 'AttributeType': 'S'},
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+    
+    startstate = "0:6,1:2"
+    dynamodb.put_item(Item={
         "GameId": {
             "S": "1"
         },
         "State": {
-            "S": "0:3,1:4,0:1,1:5,0:X;"
+            "S": startstate
         }
-    })
+    },
+    TableName=TableName
+    )
 
-def test_put_lambda():
+    #----- check if we can Update--------
+
+    p1_state = "1:6"
+
     #event where GameId is 1 and player 1 hit cup 6
     event = {
         "GameId": "1",
-        "State": "1:6"
+        "State": p1_state
     }
     response = put(event, TableName)
-    assert response == {'statusCode': "200",'body': {}}
+    assert response == {'statusCode': "200",'body': '{}'}
+
+    #----check if item was really updated------
+    
+    data = dynamodb.get_item(
+        Key={
+            "GameId": {"S": "1"}
+        },
+        TableName=TableName
+    )
+
+    assert data["Item"]["State"]["S"] ==  startstate + "," + p1_state
