fcead42f258ff5ebb23fae824840447d05d77d18
Fabian Klinger <f.klinger1@oth-aw.de>
2022-06-01 20:07:28 +0200

    added GameMenu Component and GameConnectionController

---
+++ sys-src/frontend/beerpongo-react/src/model/GameConnectionController.js
@@ -0,0 +1,15 @@
+export class GameConnectionController{
+
+
+    static tryJoiningGame(){
+
+        throw("Not implemented!");
+
+    }
+
+    static tryCreatingGame(){   
+
+        throw("Not implemented!");
+    }
+
+}
