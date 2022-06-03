737f7c71286caf2aa14fef6e8fb5cd96478e40f4
Kevin Fischer <k.fischer2@oth-aw.de>
2022-06-03 15:36:13 +0200

    added GameField to Frontend

--- sys-src/frontend/beerpongo-react/src/App.js
+++ sys-src/frontend/beerpongo-react/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import GameMenu from './components/GameMenu';
+import Field from './components/Field';
 
 function App() {
   return (
     <div className="App">
       <GameMenu></GameMenu>
+        <Field></Field>
     </div>
   );
 }
