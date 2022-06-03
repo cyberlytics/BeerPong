ed83511432fd220ce9a257af929bebe0af97667e
Kevin Fischer <k.fischer2@oth-aw.de>
2022-06-03 15:36:13 +0200

    added GameField to Frontend

---
+++ sys-src/frontend/beerpongo-react/src/components/Field.js
@@ -0,0 +1,50 @@
+import "../styles/Field.css"
+
+function Field() {
+    return (
+        <div className="desk">
+            <div className="cup-grid">
+                <div className="cup-grid-row">
+                    <div className="cup1" onClick="TODO"></div>
+                    <div className="cup1" onClick="TODO"></div>
+                    <div className="cup1"onClick="TODO"></div>
+                    <div className="cup1"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup1"onClick="TODO"></div>
+                    <div className="cup1"onClick="TODO"></div>
+                    <div className="cup1"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup1"onClick="TODO"></div>
+                    <div className="cup1"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup1"onClick="TODO"></div>
+                </div>
+            </div>
+            <div className="cup-grid cup-grid-reverse">
+                <div className="cup-grid-row">
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup2"onClick="TODO"></div>
+                    <div className="cup2"onClick="TODO"></div>
+                </div>
+                <div className="cup-grid-row">
+                    <div className="cup2"onClick="TODO"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Field;
