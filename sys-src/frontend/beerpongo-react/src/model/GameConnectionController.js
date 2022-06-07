f96d6c6a6b20add867fba9cc764d6a09615061e5
Simon Kleber <s.kleber1@oth-aw.de>
2022-06-07 18:15:06 +0200

    Add initial game requests

--- sys-src/frontend/beerpongo-react/src/model/GameConnectionController.js
+++ sys-src/frontend/beerpongo-react/src/model/GameConnectionController.js
@@ -1,15 +1,40 @@
-export class GameConnectionController{
+import axios from 'axios'
+import {GET_GAME_URL, POST_GAME_URL, PUT_GAME_URL} from "../constants/BackendUrl";
 
+export class GameConnectionController {
 
-    static tryJoiningGame(){
+  static tryCreatingGame() {
+    axios.post(POST_GAME_URL).then(data => {
+      // Todo
+      console.log(data);
+    }).catch(err => {
+      // Todo
+      console.error(err)
+    })
+  }
 
-        throw("Not implemented!");
+  static tryJoiningGame(gameId) {
+    const url = GET_GAME_URL.replace("{GAME_ID}", gameId)
 
-    }
+    axios.get(url)
+      .then(data => {
+        // Todo
+        console.log(data);
+      }).catch(err => {
+      // Todo
+      console.error(err)
+    })
+  }
 
-    static tryCreatingGame(){   
-
-        throw("Not implemented!");
-    }
+  static tryUpdatingGame(gameUpdate) {
+    axios.put(PUT_GAME_URL, gameUpdate)
+      .then(data => {
+        // Todo
+        console.log(data);
+      }).catch(err => {
+      // Todo
+      console.error(err)
+    })
+  }
 
 }
