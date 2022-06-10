2cf58b59093f36c93c57bd86a4dbd54ef585bdc0
Markus Fleischmann <m.fleischmann2@oth-aw.de>
2022-06-10 17:43:29 +0200

    Added logic for parsing the state of the game.
    Added Routes -> MenuPage and GamePage
    Rotated field 90 degrees
    Marked cups are now no longer clickable and have another background
    Added onClick for the cups

--- sys-src/frontend/beerpongo-react/src/model/GameConnectionController.js
+++ sys-src/frontend/beerpongo-react/src/model/GameConnectionController.js
@@ -1,15 +1,32 @@
 export class GameConnectionController{
 
 
-    static tryJoiningGame(){
+    static tryJoiningGame(id){
 
-        throw("Not implemented!");
-
+        throw new Error("Not implemented!");
     }
 
     static tryCreatingGame(){   
 
-        throw("Not implemented!");
+        throw new Error("Not implemented!");
+    }
+
+    static tryGettingGame(id){
+        // TODO: Replace with real implementation
+        console.log("Called tryGettingGame");
+        return "2:3X,1:5X,1:2X,1:4X,2:7X,2:5X";
+        // throw new Error("Not implemented!");
+    }
+
+    static tryUpdateGame(id, playerId, cupNr){
+        // TODO: Replace with real implementation
+        console.log("Called tryUpdateGame");
+        // throw new Error("Not implemented!");
+    }
+
+    static tryQuitGame(id){
+        console.log("Called tryQuitGame");
+        // throw new Error("Not implemented!");
     }
 
 }
