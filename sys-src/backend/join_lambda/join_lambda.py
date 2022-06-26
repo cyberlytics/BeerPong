3dba2489244b5d8bfba95ae8a90ee63e21b9617a
Fabian Klinger <f.klinger1@oth-aw.de>
2022-06-26 21:31:57 +0200

    Changed Event in Post lambda Fixed BackendUrl for frontend added Env to Dockerfile

--- sys-src/backend/join_lambda/join_lambda.py
+++ sys-src/backend/join_lambda/join_lambda.py
@@ -49,11 +49,6 @@ def join_handler(event, context):
 
     response = {
             "statusCode": "200",
-                    'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
             "body": {
                 "id": gameid, "playerid": item['playerCount']
             }
